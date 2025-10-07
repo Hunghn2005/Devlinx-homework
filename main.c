762becd624490f4fd22ddd9dc82f5ca73d094a51
Phạm Hưng <hunghn2005@gmail.com>
2025-10-07 17:09:57 +0700

    add homework lesson 1

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "strutils.h"
+
+int main(void) {
+    printf("2 + 3 = %d\n", add(2, 3));
+    printf("5 - 1 = %d\n", sub(5, 1));
+    return 0;
+}
---
+++ strutils.h
@@ -0,0 +1,5 @@
+#ifndef STRUTILS_H
+#define STRUTILS_H
+int add(int a, int b);
+int sub(int a, int b);
+#endif
